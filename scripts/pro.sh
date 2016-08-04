0dcdc8f8ec0c97a5ee1d43832b32146195f86d31
Benjamin Stein <ben@liatrio.com>
2016-08-04 13:55:53 -0700

    reference install services incorrectly

--- scripts/pro.sh
+++ scripts/pro.sh
@@ -1,4 +1,4 @@
-wget https://bintray.com/jfrog/artifactory-pro-rpms/rpm -O bintray-jfrog-artifactory-pro-rpms.repo
-sudo sudo mv bintray-jfrog-artifactory-pro-rpms.repo /etc/yum.repos.d/
-sudo yum install -y jfrog-artifactory-pro
+sudo wget https://bintray.com/jfrog/artifactory-pro-rpms/rpm -O bintray-jfrog-artifactory-pro-rpms.repo
+sudo mv bintray-jfrog-artifactory-pro-rpms.repo /etc/yum.repos.d/
+sudo yum install -y artifactory
 sudo service artifactory start
